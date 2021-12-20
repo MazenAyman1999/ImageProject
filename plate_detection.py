0e4cd2602aac608b78c7b783936336314bb86b77
Osama-Atwa <81799153+Osama-Atwa@users.noreply.github.com>
2021-12-20 08:53:36 +0200

    Update plate_detection.py

--- plate_detection.py
+++ plate_detection.py
@@ -4,17 +4,31 @@
 * @param  original img and grey scale image with intensties from 0 to 1 and image with edges
 * @return      the plate number of a car
 '''
-def Plate_detection(car_img,car_gray,car_edge):
-    car_c = binary_dilation(car_edge)
-    car_c = binary_erosion(car_c)
-    countours = np.asarray( find_contours(car_c ,level = 0.8,fully_connected='high'))
-    bounding_boxes = []
-
+def Plate_detection(car_edge,car_gray):
+        
+    arr = np.uint8(car_edge)
+    show_images([arr])
+    contours = cv.findContours(arr.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(contours)
+    contours = sorted(contours, key=cv.contourArea, reverse=True)[:10]
+    location = None
     for contour in contours:
-        approx = cv2.approxPolyDP(contour, 10, True)
+        approx = cv.approxPolyDP(contour, 10, True)
         if len(approx) == 4:
             location = approx
             break
-    mask = np.zeros(car_gray.shape, np.uint8)
-    new_image = cv2.drawContours(mask, [location], 0,255, -1)
-    new_image = cv2.bitwise_and(car_img, car_img, mask=mask)
+    Xmin = np.inf
+    Xmax = 0
+    Ymin = np.inf
+    Ymax = 0
+    for i in range(len(location)):
+        point = location[i]
+        if(point[0][0] > Xmax):
+            Xmax = point[0][0]
+        if(point[0][0] < Xmin):
+            Xmin = point[0][0]
+        if(point[0][1] > Ymax):
+            Ymax = point[0][1]
+        if(point[0][1] < Ymin):
+            Ymin = point[0][1]
+    show_images([car_gray[Ymin:Ymax,Xmin:Xmax]])
